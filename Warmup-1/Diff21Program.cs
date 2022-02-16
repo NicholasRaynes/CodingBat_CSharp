a3bdb16407fb73ebb9f5a6669a3bd8f2f34add75
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-15 18:58:06 -0600

    Completed documentation.

--- Warmup-1/Diff21Program.cs
+++ Warmup-1/Diff21Program.cs
@@ -1,7 +1,17 @@
+/*
+ * Name: Nicholas Raynes
+ * Version: 0.0.1
+ * Source: https://codingbat.com/prob/p116624
+ */
+
 using System;
 
 namespace GitHubSandbox
 {
+    /// <summary>
+    /// This class includes the declaration, implementation,
+    /// and testing for the Diff21 method.
+    /// </summary>
     public class Diff21Program
     {
         static void Main(string[] args)
@@ -14,6 +24,13 @@ namespace GitHubSandbox
             Console.ReadKey();
         }
         
+        /// <summary>
+        /// Given an int, return the absolute difference between the integer and 21, except 
+        /// return double the absolute difference if the integer is over 21.
+        /// </summary>
+        /// <param name="number">The integer input for determining the difference.</param>
+        /// <returns>The absolute difference between the integer and 21, or double the absolute 
+        /// difference if the integer is greater than 21.</returns>
         public static int Diff21(int number)
         {
             if (number <= 21)
