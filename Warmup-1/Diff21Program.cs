f3afd441bf91fc3bd088ab18b48c2b648e053977
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-15 18:54:14 -0600

    Added some testing for the Diff21 method.

--- Warmup-1/Diff21Program.cs
+++ Warmup-1/Diff21Program.cs
@@ -1,7 +1,19 @@
+using System;
+
 namespace GitHubSandbox
 {
     public class Diff21Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test #1 - Diff21(19)\nOutput: {0}\n", Diff21(19));
+            Console.WriteLine("Test #2 - Diff21(10)\nOutput: {0}\n", Diff21(10));
+            Console.WriteLine("Test #3 - Diff21(21)\nOutput: {0}\n", Diff21(21));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static int Diff21(int number)
         {
             if (number <= 21)
