82b48fbbef453e201344c0e43f065d9b11ca0d3f
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-20 23:30:38 -0600

    Create Has271Program.cs

---
+++ Warmup-2/Has271Program.cs
@@ -0,0 +1,20 @@
+namespace GitHubSandbox 
+{
+  public class Has271Program 
+  {
+    public bool Has271(int[] nums) 
+    {
+      for (int i = 0; i < (nums.Length - 2); i++) 
+      {
+        int val = nums[i];
+
+        if (nums[i + 1] == (val + 5) && Math.Abs(nums[i + 2] - (val - 1)) <= 2) 
+        {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  }
+}
