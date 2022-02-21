150b60c88b11c8fd3034319295ba4a35a3d0194f
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-20 23:37:43 -0600

    Added some testing for the method.

--- Warmup-2/Has271Program.cs
+++ Warmup-2/Has271Program.cs
@@ -2,6 +2,21 @@ namespace GitHubSandbox
 {
   public class Has271Program 
   {
+    public static void Main(string[] args)
+    {
+      // Testing 
+      int[] test1 = { 1, 2, 7, 1 };
+      int[] test2 = { 1, 2, 8, 1 };
+      int[] test3 = { 2, 7, 1 };
+
+      Console.WriteLine("Test #1 - Has271([1, 2, 7, 1])\nOutput: {0}\n", Has271(test1));
+      Console.WriteLine("Test #2 - Has271([1, 2, 8, 1])\nOutput: {0}\n", Has271(test2));
+      Console.WriteLine("Test #3 - Has271([2, 7, 1])\nOutput: {0}\n", Has271(test3));
+
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+    
     public static bool Has271(int[] nums) 
     {
       for (int i = 0; i < (nums.Length - 2); i++) 
