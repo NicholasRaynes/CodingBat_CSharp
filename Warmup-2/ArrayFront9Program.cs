99bb9bce466074b7d2edc4a7a2b1fa17ed0125c6
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-23 14:27:25 -0600

    Added some testing for the method output.

--- Warmup-2/ArrayFront9Program.cs
+++ Warmup-2/ArrayFront9Program.cs
@@ -2,8 +2,23 @@ using System;
 
 namespace GitHubSandbox
 {
-	public class ArrayFront9Program
+    public class ArrayFront9Program
     {
+	public static void Main(string[] args)
+        {
+            // Testing 
+            int[] test1 = { 1, 2, 9, 3, 4 };
+            int[] test2 = { 1, 2, 3, 4, 9 };
+            int[] test3 = { 1, 2, 3, 4, 5 };
+
+       	    Console.WriteLine("Test #1 - ArrayFront9([1, 2, 9, 3, 4])\nOutput: {0}\n", ArrayFront9(test1));
+            Console.WriteLine("Test #2 - ArrayFront9([1, 2, 3, 4, 9])\nOutput: {0}\n", ArrayFront9(test2));
+            Console.WriteLine("Test #3 - ArrayFront9([1, 2, 3, 4, 5])\nOutput: {0}\n", ArrayFront9(test3));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+	    
         public static bool ArrayFront9(int[] nums)
         {
             int arrayEnd = nums.Length;
