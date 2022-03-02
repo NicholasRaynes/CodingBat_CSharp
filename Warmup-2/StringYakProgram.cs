540df087ad26f5a33fd35f86b5cd0c7ceed9f27c
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-01 18:50:45 -0600

    Completed some output tests for the method.

--- Warmup-2/StringYakProgram.cs
+++ Warmup-2/StringYakProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class StringYakProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing 
+            Console.WriteLine("Test #1 - StringYak('yakpak')\nOutput: {0}\n", StringYak("yakpak"));
+            Console.WriteLine("Test #2 - StringYak('pakyak')\nOutput: {0}\n", StringYak("pakyak"));
+            Console.WriteLine("Test #3 - StringYak('yak123ya')\nOutput: {0}\n", StringYak("yak123ya"));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static string StringYak(string input)
         {
             string output = "";
