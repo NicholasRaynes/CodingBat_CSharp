653501c861cff6e18c8d6504d9b01f84c2fe3ebc
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-07 14:09:56 -0600

    Added some testing for the method.

--- Warmup-1/ParrotTroubleProgram.cs
+++ Warmup-1/ParrotTroubleProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class ParrotTroubleProgram
     {
+        static void Main(string[] args)
+        {
+            // Testing
+            Console.WriteLine("Test #1 - ParrotTrouble(true, 6)\nOutput: {0}\n", ParrotTrouble(true, 6));
+            Console.WriteLine("Test #2 - ParrotTrouble(true, 7)\nOutput: {0}\n", ParrotTrouble(true, 7));
+            Console.WriteLine("Test #3 - ParrotTrouble(false, 6)\nOutput: {0}\n", ParrotTrouble(false, 6));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static bool ParrotTrouble(bool isTalking, int hour)
         {
             return (isTalking && (hour < 7 || hour > 20));
