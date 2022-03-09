1b28c15d80cbcad5c93db154600cec624f123c79
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-08 19:01:15 -0600

    Completed documentation for the class and method.

--- Warmup-1/ParrotTroubleProgram.cs
+++ Warmup-1/ParrotTroubleProgram.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace GitHubSandbox
 {
+    /// <summary>
+    /// The program includes the declaration, implementation,
+    /// and testing for the ParrotTrouble method.
+    /// </summary>
     public class ParrotTroubleProgram
     {
         static void Main(string[] args)
@@ -15,6 +19,14 @@ namespace GitHubSandbox
             Console.ReadKey();
         }
         
+        /// <summary>
+        /// We have a loud talking parrot.The "hour" parameter is the current hour time in the range 0..23. We are 
+        /// in trouble if the parrot is talking and the hour is before 7 or after 20. This method returns true if 
+        /// we are in trouble.
+        /// </summary>
+        /// <param name="isTalking">Whether or not the parrot is talking.</param>
+        /// <param name="hour">The hour of a day.</param>
+        /// <returns></returns>
         public static bool ParrotTrouble(bool isTalking, int hour)
         {
             return (isTalking && (hour < 7 || hour > 20));
