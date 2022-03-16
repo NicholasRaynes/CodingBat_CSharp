1615fcd3dd51dadde05418e4b357ca6408d764c8
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-15 20:55:07 -0500

    Added some tests for the StringSplosion method.

--- Warmup-2/StringSplosionProgram.cs
+++ Warmup-2/StringSplosionProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class StringSplosionProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing 
+            Console.WriteLine("Test #1 - StringSplosion('Code')\nOutput: {0}\n", StringSplosion("Code"));
+            Console.WriteLine("Test #2 - StringSplosion('abc')\nOutput: {0}\n", StringSplosion("abc"));
+            Console.WriteLine("Test #3 - StringSplosion('Hello')\nOutput: {0}\n", StringSplosion("Hello"));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static string StringSplosion(string input)
         {
             String result = "";
