e687ee685861d9f48d4c6dfb4850387e2ec769b0
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-21 20:26:27 -0500

    Completed some testing for the method.

--- String-1/WithoutEndProgram.cs
+++ String-1/WithoutEndProgram.cs
@@ -4,9 +4,20 @@ namespace GitHubSandbox
 {
     public class WithoutEndProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing 
+            Console.WriteLine("Test #1 - WithoutEnd('Hello')\nOutput: {0}\n", WithoutEnd("Hello"));
+            Console.WriteLine("Test #2 - WithoutEnd('java')\nOutput: {0}\n", WithoutEnd("java"));
+            Console.WriteLine("Test #3 - WithoutEnd('coding')\nOutput: {0}\n", WithoutEnd("coding"));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static string WithoutEnd(string str)
         {
-            String output = "";
+            string output = "";
 
             if (str.Length > 2)
             {
