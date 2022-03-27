3c3eee328ceaf8f074bd0f8de397c6ad79ec4870
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-27 18:26:52 -0500

    Completed some tests for the method.

--- String-1/NonStartProgram.cs
+++ String-1/NonStartProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class NonStartProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing 
+            Console.WriteLine("Test #1 - NonStart('Hello', 'There')\nOutput: {0}\n", NonStart("Hello", "There"));
+            Console.WriteLine("Test #2 - NonStart('java', 'code')\nOutput: {0}\n", NonStart("java", "code"));
+            Console.WriteLine("Test #3 - NonStart('shotl', 'java')\nOutput: {0}\n", NonStart("shotl", "java"));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static string NonStart(string a, string b)
         {
             string output = "";
