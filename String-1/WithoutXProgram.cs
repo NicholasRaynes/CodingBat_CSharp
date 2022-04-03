00be52c645f448c54728b935d82a9c32de40fa8b
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-02 22:06:40 -0500

    Completed some output tests for the method.

--- String-1/WithoutXProgram.cs
+++ String-1/WithoutXProgram.cs
@@ -4,6 +4,17 @@ namespace GitHubSandbox
 {
     public class WithoutXProgram
     {
+        public static void Main(string[] args)
+        {
+            // Testing 
+            Console.WriteLine("Test #1 - WithoutX('xHix')\nOutput: {0}\n", WithoutX("xHix"));
+            Console.WriteLine("Test #2 - WithoutX('xHi')\nOutput: {0}\n", WithoutX("xHi"));
+            Console.WriteLine("Test #3 - WithoutX('Hxix')\nOutput: {0}\n", WithoutX("Hxix"));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        
         public static String WithoutX(String str)
         {
            if(str.Length > 0 && str[0] == 'x')
