a884a3e2706f4687a6ee47f58db30e11bd11d37a
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-02 21:54:38 -0500

    Update WithoutXProgram.cs

--- String-1/WithoutXProgram.cs
+++ String-1/WithoutXProgram.cs
@@ -1,1 +1,39 @@
+using System;
 
+namespace GitHubSandbox
+{
+    public class WithoutXProgram
+    {
+        public static String WithoutX(String str)
+        {
+            if(str.Length == 0)
+            {
+                return str;
+            }
+            
+            if(str.Length == 1)
+            {
+                if(str[0] == 'x')
+                {
+                    return "";
+                }
+                else
+                {
+                    return str;
+                }
+            }
+
+            if(str[0] == 'x')
+            {
+                str = str.Substring(1, str.Length);
+            }
+
+            if(str[str.Length - 1] == 'x')
+            {
+                str = str.Substring(0, str.Length - 1);
+            }
+
+            return str;
+        }
+    }
+}
