15004a52f55f5db58a8ae3374d92eee31eaea905
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-11 16:43:05 -0500

    Completed documentation for the class.

--- String-2/BobThereProgram.cs
+++ String-2/BobThereProgram.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace GitHubSandbox
 {
+    /// <summary>
+    /// The program includes the declaration, implementation,
+    /// and testing for the BobThere method.
+    /// </summary>
     public class BobThereProgram
     {
         public static void Main(string[] args)
