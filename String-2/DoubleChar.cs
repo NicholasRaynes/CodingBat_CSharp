6f354449d1d26ffd1c5cc16835074dff27631ba2
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-16 14:31:41 -0500

    Create DoubleChar.cs

---
+++ String-2/DoubleChar.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace GitHubSandbox
+{
+	public class DoubleCharProgram
+    {
+        public static string DoubleChar(string str)
+        {
+            string love = "";
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                love += str[i];
+
+                love += str[i];
+            }
+
+            return love;
+        }
+    }
+}
