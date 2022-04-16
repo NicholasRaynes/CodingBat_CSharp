9ab16228ff85fa977c79a723e7b7866e9b59d828
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-16 14:32:32 -0500

    Completed some tests for the method.

--- String-2/DoubleChar.cs
+++ String-2/DoubleChar.cs
@@ -2,6 +2,17 @@ using System;
 
 namespace GitHubSandbox
 {
+     public static void Main(string[] args)
+     {
+        // Testing 
+        Console.WriteLine("Test #1 - DoubleChar('The')\nOutput: {0}\n", DoubleChar("The"));
+        Console.WriteLine("Test #2 - DoubleChar('AAbb')\nOutput: {0}\n", DoubleChar("AAbb"));
+        Console.WriteLine("Test #3 - DoubleChar('Hi-There')\nOutput: {0}\n", DoubleChar("Hi-There"));
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+    
     public class DoubleCharProgram
     {
         public static string DoubleChar(string str)
